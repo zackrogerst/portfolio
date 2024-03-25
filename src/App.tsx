c174ad6e124ade36435adb7ffe3dfbdca6dbd883
zackrogerst <zack.rogers.t@gmail.com>
2024-03-25 02:42:22 -0600

    updated props

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 		<>
 			<Header />
 			<main style={{ height: "100vh", position: "relative" }}>
-				<Dialog headerText="Welcome To Zack OS">
+				<Dialog headerText="Welcome To Zack OS" zIndex={1}>
 					<p>
 						Hello, I'm Zack Rogers. I'm a Software Engineer based in
 						Salt Lake City, Utah who specializes in Full Stack Web
