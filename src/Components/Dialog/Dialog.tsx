e7d82265077ec004ee7c47b33a4549feeda6a7fd
zackrogerst <zack.rogers.t@gmail.com>
2024-04-03 17:16:52 -0600

    updated drag and resize, added styles

--- src/Components/Dialog/Dialog.tsx
+++ src/Components/Dialog/Dialog.tsx
@@ -1,6 +1,5 @@
-import { ReactNode, FC } from "react";
-import Draggable from "react-draggable";
-import { Resizable } from "re-resizable";
+import { ReactNode, FC, useState, useEffect } from "react";
+import { Rnd } from "react-rnd";
 
 import "./dialog.css";
 
@@ -10,36 +9,76 @@ interface DialogProps {
 	children?: ReactNode;
 }
 
-const enable = {
-	top: false,
-	right: true,
-	bottom: true,
-	left: true,
-	topRight: false,
-	bottomRight: true,
-	bottomLeft: true,
-	topLeft: false
-};
-
 const Dialog: FC<DialogProps> = ({ headerText, zIndex, children }) => {
+	const [windowSize, setWindowSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight
+	});
+
+	const [defaultPositioning, setDefaultPositioning] = useState({
+		w: windowSize.width / 4,
+		h: "auto",
+		x: windowSize.width / 2 - windowSize.width / 8, // 8 to center horizontal
+		y: windowSize.height / 2 - windowSize.height / 3
+	});
+
+	const handleClasses = {
+		bottomRight: "resize-handle",
+		bottomLeft: "resize-handle"
+	};
+
+	const defaultPositionSize = {
+		x: defaultPositioning.x,
+		y: defaultPositioning.y,
+		width: defaultPositioning.w,
+		height: defaultPositioning.h
+	};
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight
+			});
+			setDefaultPositioning({
+				w: windowSize.width / 4,
+				h: "auto",
+				x: windowSize.width / 2 - windowSize.width / 2 / 2 / 2,
+				y: windowSize.height / 2 - windowSize.height / 2 / 2
+			});
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, [window.innerHeight, window.innerWidth]);
+
 	return (
-		<Draggable handle="strong" bounds="html">
-			<Resizable
-				bounds={"window"}
-				enable={enable}
-				className="dialog-container"
-				defaultSize={{ width: 600, height: "auto" }}
+		<Rnd
+			resizeHandleClasses={handleClasses}
+			dragHandleClassName="drag-handle"
+			bounds={"window"}
+			className="rnd-container"
+			default={defaultPositionSize}
+			minHeight={100}
+		>
+			<div
+				style={{
+					height: "100%",
+					overflow: "auto",
+					scrollbarWidth: "thin",
+					scrollbarGutter: "stable"
+				}}
 			>
-				<div style={{ zIndex: zIndex }}>
-					<strong className="cursor">
-						<div className="dialog-header">
-							<h3>{headerText}</h3>
-						</div>
-					</strong>
-					<div className="dialog-content">{children}</div>
+				<div className="drag-handle dialog-header" style={{ zIndex: zIndex }}>
+					<h3>{headerText}</h3>
 				</div>
-			</Resizable>
-		</Draggable>
+
+				<div className="dialog-content">{children}</div>
+			</div>
+		</Rnd>
 	);
 };
 
