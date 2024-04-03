f30fea662bc336d72a5ae5db38cb173f53b01186
zackrogerst <zack.rogers.t@gmail.com>
2024-04-02 21:12:43 -0600

    added resize

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,16 @@ import Dialog from "./Components/Dialog/Dialog";
 function App() {
 	return (
 		<>
-			<main style={{ height: "100vh", position: "relative" }}>
+			<main
+				style={{
+					height: "100vh",
+					position: "relative",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					overflow: "hidden"
+				}}
+			>
 				<Dialog headerText="Welcome To Zack OS" zIndex={1}>
 					<p>
 						Hello, I'm Zack Rogers. I'm a Software Engineer based in Salt Lake City,
--- src/Components/Dialog/Dialog.tsx
+++ src/Components/Dialog/Dialog.tsx
@@ -1,5 +1,7 @@
 import { ReactNode, FC } from "react";
 import Draggable from "react-draggable";
+import { Resizable } from "re-resizable";
+
 import "./dialog.css";
 
 interface DialogProps {
@@ -8,17 +10,35 @@ interface DialogProps {
 	children?: ReactNode;
 }
 
+const enable = {
+	top: false,
+	right: true,
+	bottom: true,
+	left: true,
+	topRight: false,
+	bottomRight: true,
+	bottomLeft: true,
+	topLeft: false
+};
+
 const Dialog: FC<DialogProps> = ({ headerText, zIndex, children }) => {
 	return (
-		<Draggable handle="strong" bounds="parent">
-			<div className="dialog-container" style={{ zIndex: zIndex }}>
-				<strong className="cursor">
-					<div className="dialog-header">
-						<h3>{headerText}</h3>
-					</div>
-				</strong>
-				<div className="dialog-content">{children}</div>
-			</div>
+		<Draggable handle="strong" bounds="html">
+			<Resizable
+				bounds={"window"}
+				enable={enable}
+				className="dialog-container"
+				defaultSize={{ width: 600, height: "auto" }}
+			>
+				<div style={{ zIndex: zIndex }}>
+					<strong className="cursor">
+						<div className="dialog-header">
+							<h3>{headerText}</h3>
+						</div>
+					</strong>
+					<div className="dialog-content">{children}</div>
+				</div>
+			</Resizable>
 		</Draggable>
 	);
 };
